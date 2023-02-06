b491e058efeb79c4af4ce85b1b8fcee1ea057330
叶偲诚 <yesicheng@onemt.com.cn>
2023-02-06 19:33:47 +0800

    feat:新增AssetRes加载asset资源

---
+++ ResourceManagerDemo/Assets/QFramework/Framework/ResKit/AssetRes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+namespace QFramework
+{
+    public class AssetRes : Res
+    {
+        private string mOwnerBundleName;
+        public AssetRes(string assetName,string ownerBundleName)
+        {
+            Name = assetName; 
+            mOwnerBundleName = ownerBundleName;
+            ResState = ResState.Waiting;
+        }
+        private ResLoader mResLoader = new ResLoader();
+        public override void LoadAsync()
+        {
+            ResState = ResState.Loading;
+            mResLoader.LoadAsync<AssetBundle>(mOwnerBundleName, (ownerBundle) =>
+            {
+                AssetBundleRequest assetBundleRequest = ownerBundle.LoadAssetAsync(Name);
+                assetBundleRequest.completed += operation =>
+                {
+                    Asset = assetBundleRequest.asset;
+                    ResState = ResState.loaded;
+                };
+            });
+        }
+
+        public override bool LoadSync()
+        {
+            ResState = ResState.Loading;
+            var assetBundle =  mResLoader.LoadSync<AssetBundle>(mOwnerBundleName);
+            Asset =  assetBundle.LoadAsset(Name);
+            ResState = ResState.loaded;
+            return Asset;
+        }
+
+        protected override void OnReleaseRes()
+        {
+            if(Asset is GameObject)
+            {
+
+            }
+            else
+            {
+                Resources.UnloadAsset(Asset);
+                Asset = null;
+            }
+            mResLoader.ReleaseAll();
+            mResLoader = null;
+
+            ResMgr.Instance.SharedLoadedReses.Remove(this);
+        }
+    }
+}
