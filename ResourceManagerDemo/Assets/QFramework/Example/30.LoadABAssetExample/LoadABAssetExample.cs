669bc1bb1381391345bce657a94465ae0ad53dd7
叶偲诚 <yesicheng@onemt.com.cn>
2023-02-07 23:23:13 +0800

    feat:AssetLoad测试用例

---
+++ ResourceManagerDemo/Assets/QFramework/Example/30.LoadABAssetExample/LoadABAssetExample.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using ET;
+
+namespace QFramework
+{
+	public class LoadABAssetExample : MonoBehaviour
+	{
+
+#if UNITY_EDITOR
+		[UnityEditor.MenuItem("QFramework/Example/30.LoadABAssetExample", false, 30)]
+		static void MenuClicked()
+		{
+			UnityEditor.EditorApplication.isPlaying = true;
+
+			new GameObject("LoadABAssetExample")
+				.AddComponent<LoadABAssetExample>();
+		}
+#endif
+
+		private ResLoader mResLoader = new ResLoader();
+		
+		// Use this for initialization
+		void Start()
+		{
+			var squareTexture = mResLoader.LoadSync<Texture2D>("square", "Square");
+			Debug.Log(squareTexture.name);
+			
+			mResLoader.LoadAsync<GameObject>("gameobject","GameObject",
+				gameObjPrefab => { Instantiate(gameObjPrefab); });
+		}
+
+		private void OnDestroy()
+		{
+			mResLoader.ReleaseAll();
+			mResLoader = null;
+		}
+	}
+}
